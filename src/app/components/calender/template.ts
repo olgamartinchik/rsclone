5f1c23d75b6c59e8060b499369edc3cfde565a5b
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-01-31 21:28:25 +0300

    fix: fix adaptivity

--- src/app/components/calender/template.ts
+++ src/app/components/calender/template.ts
@@ -1,7 +1,7 @@
 export default function calenderTemplate(): string {
     return `
   <span class="title title-tablet">Birthday</span>
-  <input type="text" class="datepicker" name="datepicker" placeholder="January 01, 1990" />
+  <input type="text" class="datepicker" name="datepicker" placeholder="Jan 01, 1990" />
   <span class="calender"></span>
   `;
 }
