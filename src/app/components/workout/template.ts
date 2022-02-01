352d2185d288cdb222614d6caea17b22c81b9571
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-02-01 15:59:31 +0200

    feat: add basic rendering

--- src/app/components/workout/template.ts
+++ src/app/components/workout/template.ts
@@ -1,43 +1,21 @@
-export default function workoutTemplate() {
-     return `<main class="main-page">
-            <div class="workout-header">
+export default function workoutHeaderTemplate(title: string, intensity: string, duration: number, type: string): string {
+     return `<div class="workout-header">
                 <div class="workout-container">
                     <div class="workout-info">
-                        <h1 class="workout-title">Toned Arms & Back</h1>
+                        <h1 class="workout-title">${title}</h1>
                         <div class="workout-types">
-                            <span class="workout-type">HIIT</span>
+                            <span class="workout-type">${type}</span>
                         </div>
                         <div class="parameters">
-                            <span>10 min</span>
+                            <span>${Math.round(duration / 60)} min</span>
                             <span class="separator">|</span>
                             <span class="right-space">Intensity</span>
                             <span class="middle-dot active"></span>
-                            <span class="middle-dot active"></span>
-                            <span class="middle-dot"></span>
+                            <span class="middle-dot ${intensity === 'medium' || intensity === 'high' ? 'active' : ''}"></span>
+                            <span class="middle-dot ${intensity === 'high' ? 'active' : ''}"></span>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <div class="workout-details">
-                <div class="workout-container">
-                    <div class="workout-controls">
-                        <button class="workout-fav">
-                            <span>favourite</span>
-                            <i class="far fa-heart"></i>
-                        </button>
-                        <button class="waves-effect waves-light btn-large">Start</button>
-                    </div>
-                </div>
-                <div class="workout-container workout-container-sm">
-                    <div class="workout-description">
-                        <p class="workout-text">Sculpt and strengthen your entire upper body with this incredible arms and back workout. Use your own body
-                            weight or
-                            grab a set of dumbbells to challenge yourself.</p>
-                        <p class="workout-text"><span class="workout-text-type">Equipment</span>dumbbells</p>
-                    </div>
-                </div>
-            </div>
-        </main>
         `;
 }
--- src/app/pages/workoutPage/workoutPageView.ts
+++ src/app/pages/workoutPage/workoutPageView.ts
@@ -1,6 +1,10 @@
+import Button from '../../components/Button';
 import footer from '../../components/footer/footer';
 import header from '../../components/header/header';
 import NavBar from '../../components/header/navbar';
+import Node from '../../components/Node';
+import workoutHeaderTemplate from '../../components/workout/template';
+import workoutDesc from '../../components/workout/workoutDescription';
 
 class WorkoutPageView {
     private rootNode: HTMLElement;
@@ -22,13 +26,35 @@ class WorkoutPageView {
         ]);
         navbar.generateMenu();
         navbar.addProfileLink('O');
-        this.rootNode.insertAdjacentHTML(
-            'beforeend',
-            `<main class="main-page"><span>This is Workout Page for training with id ${id}!</span></main>`
-        );
-
+        this.createMainBlockLayout();
+        
         this.rootNode.append(footer.getTemplate());
     }
+
+    private createMainBlockLayout(): void {
+        const mainPage = new Node(this.rootNode, 'main', 'main-page');
+        mainPage.node.insertAdjacentHTML('afterbegin', workoutHeaderTemplate('', '', 23, ''));
+        mainPage.node.append(this.getWorkoutDetailsLayout());
+    }
+
+    private getWorkoutDetailsLayout(): HTMLElement {
+        const workoutDetails = new Node(null, 'div', 'workout-details');
+        const workoutContainer = new Node(workoutDetails.node, 'div', 'workout-container');
+        const workoutContainerSm = new Node(workoutDetails.node, 'div', 'workout-container workout-container-sm');
+        const controls = new Node(workoutContainer.node, 'div', 'workout-controls');
+        const buttonFav = new Node(controls.node, 'button', 'workout-fav');
+        buttonFav.node.insertAdjacentHTML(
+            'afterbegin',
+            `<span>favourite</span>
+            <i class="far fa-heart"></i>`
+        );
+        const buttonStart = new Button(controls.node, 'Start');
+        buttonStart.button.node.className = 'waves-effect waves-light btn-large';
+        workoutContainerSm.node.insertAdjacentHTML('afterbegin', workoutDesc('', ''));
+
+        return workoutDetails.node;
+    }
+
 }
 
 export default WorkoutPageView;
