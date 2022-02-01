9fbaf20979fa16a985890d1af7062892b828c4f6
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-02-01 12:41:04 +0200

    feat: add responsive layout

--- src/app/pages/workoutPage/workoutPageController.ts
+++ src/app/pages/workoutPage/workoutPageController.ts
@@ -1,9 +1,12 @@
+import WorkoutPageModel from './workoutPageModel';
 import WorkoutPageView from './workoutPageView';
 
 class WorkoutPageController {
     private view: WorkoutPageView;
+    private model: WorkoutPageModel;
 
     constructor() {
+        this.model = new WorkoutPageModel();
         this.view = new WorkoutPageView();
     }
 
