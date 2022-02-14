b2c03034c08838055f43a00d46893c9f75b89438
olgamartinchik <sintcerova@mail.ru>
2022-02-14 16:33:54 +0300

    feat: add resipe page with recipe data

--- src/app/services/types.ts
+++ src/app/services/types.ts
@@ -70,7 +70,9 @@ export type RouteOption = {
     callback: () => void;
     isAuth: boolean | null;
 };
-
+export type TIngredients={
+    image:string
+}
 export interface IDataExplore {
     recipe: {
         image?: string;
@@ -81,6 +83,12 @@ export interface IDataExplore {
         yield?: number;
         diet?: string;
         dietLabels?: string;
+        ingredients?:Array<TIngredients>
+        url?:string,
+        ingredientLines?:Array<string>,
+        totalTime?:string,
+        totalWeight?:string,
+        healthLabels?:Array<string>
     };
 }
 
