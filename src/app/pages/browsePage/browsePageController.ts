6825406711833ae8ba02aa58fb4629e42ca0405b
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-02-17 08:23:14 +0300

    feat: add rendering of different headers on browse page for loggedIn and not users

--- src/app/pages/browsePage/browsePageController.ts
+++ src/app/pages/browsePage/browsePageController.ts
@@ -1,4 +1,7 @@
 import BrowsePageView from './browsePageView';
+import StorageManager from '../../services/storageManager';
+import authManager from '../../services/authManager';
+import { TToken, TSettings } from '../../services/types';
 
 class BrowsePageController {
     private view: BrowsePageView;
@@ -8,7 +11,24 @@ class BrowsePageController {
     }
 
     public createPage() {
-        this.view.render();
+        const isLogin = this.checkAuth();
+        this.view.render(isLogin, this.signUpHandler.bind(this));
+    }
+
+    private checkAuth(): boolean {
+        let isLogin = false;
+        const token = <TToken>StorageManager.getItem('token', 'local');
+        const userSettings = <TSettings>StorageManager.getItem('userSettings', 'local');
+
+        if (token && userSettings) {
+            isLogin = true;
+        }
+
+        return isLogin;
+    }
+
+    private signUpHandler(): void {
+        authManager.navigate('/register');
     }
 }
 
--- src/app/pages/browsePage/browsePageView.ts
+++ src/app/pages/browsePage/browsePageView.ts
@@ -10,7 +10,17 @@ class BrowsePageView {
         this.rootNode = <HTMLElement>document.getElementById('app');
     }
 
-    render() {
+    public render(isLogin: boolean, signUpHandler: () => void) {
+        if (isLogin) {
+            this.createAuthorizedHeader()
+        } else {
+            this.createNotAuthorizedHeader(signUpHandler);
+        }
+        this.createContent();
+        this.rootNode.append(footer.getTemplate());
+    }
+
+    private createAuthorizedHeader(): void {
         this.rootNode.textContent = '';
         this.rootNode.append(header.getTemplate());
         const user = <string>storageManager.getItem('user', 'local');
@@ -23,12 +33,25 @@ class BrowsePageView {
         ]);
         navbar.generateMenu('Browse');
         navbar.addProfileLink(user);
+    }
+
+    private createNotAuthorizedHeader(signUpHandler: () => void): void {
+        this.rootNode.textContent = '';
+        this.rootNode.append(header.getTemplate());
+
+        const navWrapper = this.rootNode.querySelector('.nav-wrapper') as HTMLElement;
+        const navbar = new NavBar(navWrapper, ['Team', 'Browse', 'Login'], true);
+        navbar.generateMenu();
+        if (navbar.button) {
+            navbar.button.button.node.onclick = () => signUpHandler();
+        }
+    }
+
+    private createContent(): void {
         this.rootNode.insertAdjacentHTML(
             'beforeend',
             `<main class="main-layout"><span>This is Browse Page!</span></main>`
         );
-
-        this.rootNode.append(footer.getTemplate());
     }
 }
 
