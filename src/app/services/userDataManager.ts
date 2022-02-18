4e7f6f5b599f0083d0f4740608040356df10f5ff
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-02-18 12:10:19 +0300

    feat: add workoutmanager to userdatamanager

--- src/app/services/userDataManager.ts
+++ src/app/services/userDataManager.ts
@@ -1,13 +1,16 @@
 import MealPageModel from '../pages/mealPage/mealPageModel';
 import CalculationCalories from './calculationCalories';
 import DateManager from './datesManager';
+import WorkoutManager from './workoutManager';
 import { TSettings } from './types';
 
 class UserDataManager {
     userSettings: TSettings;
+    wrManager: WorkoutManager;
 
     constructor(userSettings: TSettings) {
         this.userSettings = userSettings;
+        this.wrManager = new WorkoutManager();
     }
 
     async createUserData() {
@@ -16,6 +19,7 @@ class UserDataManager {
         new DateManager().getArrayDate(this.userSettings);
         const numWeek = new DateManager().getNumWeek(this.userSettings);
         await new MealPageModel().getUserMealData();
+        this.wrManager.getProgram(this.userSettings);
     }
 }
 export default UserDataManager;
